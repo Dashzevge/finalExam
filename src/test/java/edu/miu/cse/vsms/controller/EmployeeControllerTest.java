cb8556564ba14b231406c10d6fd581e886fb1290
Dashzevge <76143002+Dashzevge@users.noreply.github.com>
2024-11-25 12:08:19 -0600

    third fix  commit unit test

--- src/test/java/edu/miu/cse/vsms/controller/EmployeeControllerTest.java
+++ src/test/java/edu/miu/cse/vsms/controller/EmployeeControllerTest.java
@@ -4,13 +4,19 @@ import com.google.gson.Gson;
 import edu.miu.cse.vsms.dto.request.EmployeeRequestDto;
 import edu.miu.cse.vsms.dto.response.EmployeeResponseDto;
 import edu.miu.cse.vsms.service.EmployeeService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,29 +25,25 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
 
-@WebMvcTest(EmployeeController.class)
+@ExtendWith(MockitoExtension.class)
 public class EmployeeControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
+    @Mock
+    private EmployeeService service;
 
-    @MockBean
-    private EmployeeService employeeService;
+    @InjectMocks
+    private EmployeeController employeeController;
 
     @Test
     void createEmployee() throws Exception {
         EmployeeRequestDto employeeRequestDto = new EmployeeRequestDto("john","john@gmail","1923123", LocalDate.of(1990,10,23));
+        long employeeId = 1L;
+        EmployeeResponseDto employeeResponseDto = new EmployeeResponseDto( employeeId, "john", "john@gmail","1923123", LocalDate.of(1990,10,23),null);
 
-        EmployeeResponseDto employeeResponseDto = new EmployeeResponseDto( 1, "john", "john@gmail","1923123", LocalDate.of(1990,10,23),null);
+        Mockito.when(service.addEmployee(employeeRequestDto)).thenReturn(employeeResponseDto);
 
-        Mockito.when(employeeService.addEmployee(employeeRequestDto)).thenReturn(employeeResponseDto);
+        ResponseEntity<EmployeeResponseDto> responseEntity = employeeController.addEmployee(employeeRequestDto);
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/employees")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(new Gson().toJson(employeeRequestDto))
-        )
-                .andDo(MockMvcResultHandlers.print())
-                .andExpectAll(
-                        MockMvcResultMatchers.content().json(new Gson().toJson(employeeResponseDto))
-                );
+        Assertions.assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        Assertions.assertEquals(employeeResponseDto, responseEntity.getBody());
     }
 }
